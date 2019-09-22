3653f77fa6419fdefedeecbe2492745dd2f05426
Anton Christensen <anton.christensen9700@gmail.com>
2019-09-22 14:22:34 +0200

    Updates config parameter name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ -w /etc/pushpin/pushpin.conf ]; then
 		-e 's/services=.*/services=mongrel2,m2adapter,zurl,pushpin-proxy,pushpin-handler/' \
 		-e 's/push_in_spec=.*/push_in_spec=tcp:\/\/\*:5560/' \
 		-e 's/push_in_http_addr=.*/push_in_http_addr=0.0.0.0/' \
-		-e 's/push_in_sub_spec=.*/push_in_sub_spec=tcp:\/\/\*:5562/' \
+		-e 's/push_in_sub_specs=.*/push_in_sub_spec=tcp:\/\/\*:5562/' \
 		-e 's/command_spec=.*/command_spec=tcp:\/\/\*:5563/' \
 		-e 's/^http_port=.*/http_port=7999/' \
 		/etc/pushpin/pushpin.conf
